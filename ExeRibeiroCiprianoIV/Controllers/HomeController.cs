023b2d26d0b8eddc2ae57f6ebc98df9427727ba3
Gui-Ribs <gr.estudios8@gmail.com>
2023-06-19 04:08:22 -0300

    finished

---
+++ ExeRibeiroCiprianoIV/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using ExeRibeiroCiprianoIV.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExeRibeiroCiprianoIV.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ClienteCadastrado(Cliente cliente)
+        {
+             return View(cliente);
+        }
+
+        public ActionResult CheckLogin(string Login)
+        {
+            var dbCliente = new Collection<string>
+            {
+               "Prático",
+               "Heitor",
+               "Cícero"
+            };
+            return Json(dbCliente.All(a => a.ToLower() != Login.ToLower()), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult CheckCPF(long CPF)
+        {
+            var dbCliente = new Collection<long>
+            {
+               11122233311,
+               11122233312,
+               11122233313
+            };
+            return Json(dbCliente.All(a => a != CPF), JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ ExeRibeiroCiprianoIV/Models/Cliente.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ExeRibeiroCiprianoIV.Models
+{
+    public class Cliente
+    {
+        [DisplayName("Código do cliente")]
+        [Required(ErrorMessage ="O código é obrigatório")]
+        public int IdCli { get; set; }
+
+        [DisplayName("Nome do cliente")]
+        [Required(ErrorMessage ="O nome é obrigatório")]
+        [StringLength(150, ErrorMessage ="O campo deve conter no máximo 150 caracteres")]
+        public string NomeCli { get; set; }
+
+        [System.Web.Mvc.Remote("CheckCPF", "Home", ErrorMessage = "O CPF já esxiste")]
+        [DisplayName("CPF")]
+        [Required(ErrorMessage = "Informe o CPF")]
+        public long CPF { get; set; }
+
+        [DisplayName("E-mail")]
+        [RegularExpression(@"^[a - zA - Z] + (([\'\,\.\-][a-zA-Z ])?[a-zA-Z]*)*\s+<(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})>$|^(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})$",
+            ErrorMessage ="Digite um e-mail válido")]
+        [Required(ErrorMessage = "O e-mail é obrigatório")]
+        public string Email { get; set; }
+
+        [System.Web.Mvc.Remote("CheckLogin", "Home", ErrorMessage = "O Login já esxiste")]
+        [DisplayName("Login")]
+        [Required(ErrorMessage = "O login é obrigatório")]
+        [StringLength(15, ErrorMessage = "O campo deve conter no máximo 15 caracteres")]
+        public string Login { get; set; }
+
+        [DataType(DataType.Password)]
+        [DisplayName("Senha")]
+        [Required(ErrorMessage = "A senha é obrigatória")]
+        public string Senha { get; set; }
+
+        [DataType(DataType.Password)]
+        [DisplayName("Confirme a senha")]
+        [Compare("Senha", ErrorMessage ="As senhas são diferentes")]
+        public string ConfirmSenha { get; set; }
+
+        [DisplayName("Data do cadastro")]
+        [Required(ErrorMessage = "A data é obrigatória")]
+        public string DataCadastro { get; set; }
+    }
+}
